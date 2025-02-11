832758c1bcebcbcdcdf91ca40eda71d3629fd489
yknNEU <181112251+yknNEU@users.noreply.github.com>
2025-02-10 20:37:28 -0500

    Finish Assignment 3

--- src/main/java/com/phasmidsoftware/dsaipg/sort/elementary/InsertionSortComparator.java
+++ src/main/java/com/phasmidsoftware/dsaipg/sort/elementary/InsertionSortComparator.java
@@ -10,7 +10,9 @@ import com.phasmidsoftware.dsaipg.util.Config;
 import com.phasmidsoftware.dsaipg.util.Config_Benchmark;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 
 import static com.phasmidsoftware.dsaipg.sort.InstrumentedComparatorHelper.getRunsConfig;
 
@@ -64,9 +66,15 @@ public class InsertionSortComparator<X> extends SortWithHelper<X> {
      */
     public void sort(X[] xs, int from, int to) {
         final Helper<X> helper = getHelper();
-
-        // TO BE IMPLEMENTED 
-throw new RuntimeException("implementation missing");
+        for (int i = from + 1; i < to; i++) {
+            X key = xs[i];
+            int j = i - 1;
+            while (j >= from && helper.compare(xs[j], key) > 0) {
+                xs[j + 1] = xs[j];
+                j--;
+            }
+            xs[j + 1] = key;
+        }
     }
 
     public static final String DESCRIPTION = "Insertion sort";
@@ -111,6 +119,107 @@ throw new RuntimeException("implementation missing");
             Helper<T> helper = sorter.getHelper();
             sorter.sort(ts, true);
             return helper.getFixes();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Benchmarking insertion sort with integers
+        Random random = new Random();
+        int[] n_times = {1000, 2000, 4000, 8000, 16000, 32000, 64000};
+
+        // Init warm up JVM
+        long startTime = System.nanoTime();
+        int temp = 0;
+        for (int i = 0; i < 1000000; i++) {
+            temp = random.nextInt();
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Warm up Finished: " + temp);
+
+        for (int n : n_times) {
+            // Generate an array of n random integers
+            int[] inputRandom = new int[n];   // Ramdon array to be sorted
+            int[] inputOrdered = new int[n];  // Already ordered array to be sorted
+            int[] inputPartlyOrdered = new int[n];  // Half part of this array is ordered
+            int[] inputReversed = new int[n];  // Reversed array to be sorted
+
+            for (int i = 0; i < n; i++) {
+                inputRandom[i] = random.nextInt();
+                inputOrdered[i] = i;
+                if (i < n / 2) {
+                    inputPartlyOrdered[i] = i;
+                } else {
+                    inputPartlyOrdered[i] = Math.abs(random.nextInt() / 2) + (n / 2);
+                }
+                inputReversed[i] = n - i;
+            }
+
+            // Warm up after data generation
+            startTime = System.nanoTime();
+            for (int i = 0; i < 1000000; i++) {
+                temp = random.nextInt();
+            }
+            endTime = System.nanoTime();
+
+            // Benchmarking insertion sort with random integers
+            startTime = System.nanoTime();
+            // Do 10 times sort to warm up the JVM
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputRandom).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            // Do 10 times sort to get the average time
+            startTime = System.nanoTime();
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputRandom).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            System.out.println("Insertion sort with random integers, n = " + n + ": " + (endTime - startTime) / 10 + " ns");
+
+            // Benchmarking insertion sort with ordered integers
+            startTime = System.nanoTime();
+            // Do 10 times sort to warm up the JVM
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputOrdered).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            // Do 10 times sort to get the average time
+            startTime = System.nanoTime();
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputOrdered).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            System.out.println("Insertion sort with ordered integers, n = " + n + ": " + (endTime - startTime) / 10 + " ns");
+
+            // Benchmarking insertion sort with partly ordered integers
+            startTime = System.nanoTime();
+            // Do 10 times sort to warm up the JVM
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputPartlyOrdered).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            // Do 10 times sort to get the average time
+            startTime = System.nanoTime();
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputPartlyOrdered).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            System.out.println("Insertion sort with partly ordered integers, n = " + n + ": " + (endTime - startTime) / 10 + " ns");
+
+            // Benchmarking insertion sort with reversed integers
+            startTime = System.nanoTime();
+            // Do 10 times sort to warm up the JVM
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputReversed).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            // Do 10 times sort to get the average time
+            startTime = System.nanoTime();
+            for (int i = 0; i < 10; i++) {
+                InsertionSortComparator.sort(Arrays.stream(inputReversed).boxed().toArray(Integer[]::new));
+            }
+            endTime = System.nanoTime();
+            System.out.println("Insertion sort with reversed integers, n = " + n + ": " + (endTime - startTime) / 10 + " ns");
         }
     }
 
--- src/main/java/com/phasmidsoftware/dsaipg/util/Timer.java
+++ src/main/java/com/phasmidsoftware/dsaipg/util/Timer.java
@@ -64,9 +64,25 @@ public class Timer {
      * @return the average milliseconds per repetition.
      */
     public <T, U> double repeat(int n, boolean warmup, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
-        // TO BE IMPLEMENTED : note that the timer is running when this method is called and should still be running when it returns.
-         return 0;
-        // END SOLUTION
+        for (int i = 0; i < n; i++) {
+            T input = supplier.get();
+            if (preFunction != null) {
+                input = preFunction.apply(input);
+            }
+            long start = getClock();
+            U result = function.apply(input);
+            if (postFunction != null) {
+                postFunction.accept(result);
+            }
+            lap();
+            if (warmup) continue;
+            long end = getClock();
+            ticks += end - start;
+        }
+        pause();
+        final double result = meanLapTime();
+        resume();
+        return result;
     }
 
     /**
@@ -239,9 +255,7 @@ public class Timer {
      * @return the number of ticks for the system clock. Currently defined as nano time.
      */
     private static long getClock() {
-        // TO BE IMPLEMENTED 
-         return 0;
-        // END SOLUTION
+        return System.nanoTime();
     }
 
     /**
@@ -252,9 +266,7 @@ public class Timer {
      * @return the corresponding number of milliseconds.
      */
     private static double toMillisecs(long ticks) {
-        // TO BE IMPLEMENTED 
-         return 0;
-        // END SOLUTION
+        return ticks * 1.0E-6;
     }
 
     final static LazyLogger logger = new LazyLogger(Timer.class);
